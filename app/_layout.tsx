8e0ee9ea915c3ee094b03b19c74d0cf9195f3e2c
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-29 18:10:30 +0100

    refactor: added provider and modified scrollView for flatlists

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,24 @@
 import { Stack } from 'expo-router';
 import React from 'react';
 import MainHeader from '../components/MainHeader/MainHeader';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
 
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          title: 'home',
-          headerTitle: 'home',
-          headerShown: true,
-          header: () => <MainHeader />,
-        }}
-      />
-    </Stack>
+    <Provider store={store}>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            title: 'home',
+            headerTitle: 'home',
+            headerShown: true,
+            header: () => <MainHeader />,
+          }}
+        />
+      </Stack>
+    </Provider>
   );
 };
 
