22995c4a57878311e22ec82ba6e0142b8307c08c
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-03-13 12:54:22 +0100

    feat: implemented total price attribute in cart slice

--- Slices/cartSlice.ts
+++ Slices/cartSlice.ts
@@ -1,33 +1,49 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { IProduct, IProductCart } from '../Interfaces/IProducts';
 
-export interface ICartState extends IProduct {
-  quantity: number;
+export interface ICartState {
+  products: Array<IProduct & { quantity: number }>;
+  total: number;
 }
 
-const initialState: ICartState[] = [];
+const initialState: ICartState = {
+  products: [],
+  total: 0,
+};
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: initialState,
   reducers: {
     addProductToCart: (state, action: PayloadAction<IProduct>) => {
-      const existingProduct = state.find(
+      const existingProduct = state.products.find(
         (product) => product.id === action.payload.id
       );
+
       if (existingProduct) {
         existingProduct.quantity += 1;
-      } else return [...state, { ...action.payload, quantity: 1 }];
+        state.total += existingProduct.price;
+      } else
+        return {
+          products: [...state.products, { ...action.payload, quantity: 1 }],
+          total: state.total + action.payload.price,
+        };
     },
     removeProductFromCart: (state, action: PayloadAction<IProductCart>) => {
-      const existingProduct = state.find(
+      const existingProduct = state.products.find(
         (product) => product.id === action.payload.id
       );
 
       if (existingProduct?.quantity === 1) {
-        return state.filter((product) => product.id !== action.payload.id);
+        return {
+          products: state.products.filter(
+            (product) => product.id !== action.payload.id
+          ),
+          total: state.total - existingProduct.price,
+        };
       } else {
         existingProduct ? (existingProduct.quantity -= 1) : '';
+        state.total -= existingProduct ? existingProduct.price : 0;
       }
     },
     deleteCart: () => {
