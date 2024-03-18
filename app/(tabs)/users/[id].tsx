38720a929964b0525e18d9a55fc5da94b7ed1422
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-03-18 17:29:25 +0100

    feat: implmented the modal in this screen

--- app/(tabs)/users/[id].tsx
+++ app/(tabs)/users/[id].tsx
@@ -1,12 +1,42 @@
 import { useLocalSearchParams } from 'expo-router';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import LogSignInFormModal from '../../../components/LogSignInFormModal';
+import { IUser } from '../../../Interfaces/IUser';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const UserPage = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+  const [user, setUser] = useState<IUser>({ name: '', password: '' });
+  const handleVisibility = useCallback(() => {
+    setIsModalVisible((prev) => !prev);
+  }, [isModalVisible]);
+
+  const getCurrentlyLoggedUser = useCallback(async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('loggedUser');
+      jsonValue != null ? setUser(JSON.parse(jsonValue)) : null;
+    } catch (e) {
+      setError(true);
+    }
+  }, [user]);
+  useEffect(() => {
+    getCurrentlyLoggedUser();
+  }, [user]);
+
+  if (error) return <Text>Uuups we had an error!</Text>;
   return (
     <View>
-      <Text>user : {id} </Text>
+      <TouchableOpacity onPress={handleVisibility}>
+        <Text> Show Form</Text>
+      </TouchableOpacity>
+      <LogSignInFormModal
+        handleVisibility={handleVisibility}
+        isModalVisible={isModalVisible}
+      />
+      <Text>{user.name}</Text>
     </View>
   );
 };
