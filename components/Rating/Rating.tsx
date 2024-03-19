0881699a5d11a7865709971d67e1bdddae587a09
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-03-19 14:55:33 +0100

    refactor: refactored createStar function to look less '80 style

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -7,29 +7,22 @@ interface IRatingProps {
 }
 
 const Rating = ({ rating }: IRatingProps) => {
-  const decimal = rating - Math.floor(rating);
-
   const createStars = useMemo(() => {
-    const star = [];
-    let name = '';
-    for (let i = 1; i < 6; i++) {
-      if (rating > i) name = 'star';
-      else if (decimal > 0.49) name = 'star-half-o';
-      else name = 'star-o';
-      star.push(
+    return Array.from({ length: 5 }, (_, index) => {
+      let name = 'star-o';
+      if (rating > index + 1) name = 'star';
+      else if (rating - Math.floor(rating) > 0.49) name = 'star-half-o';
+      return (
         <FontAwesome
-          key={i}
+          key={index}
           name={name as never}
           size={24}
           color={COLORS.yellow}
         />
       );
-    }
-    return star;
+    });
   }, [rating]);
   return createStars;
 };
-
-//TODO: cercare metodo migliore
 
 export default Rating;
