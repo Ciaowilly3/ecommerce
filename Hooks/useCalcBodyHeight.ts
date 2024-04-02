acf19c2aec496977842b5f24b435e57642db6ef6
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-04-02 16:11:40 +0200

    refactor: refactored hook to handle android devices

--- Hooks/useCalcBodyHeight.ts
+++ Hooks/useCalcBodyHeight.ts
@@ -1,14 +1,14 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, Platform, StatusBar } from 'react-native';
 import { useHeaderHeight } from '@react-navigation/elements';
 import { useCallback } from 'react';
 
 const useCalcBodyHeight = (footerHeight: number) => {
   const windowHeight = Dimensions.get('window').height;
   const headerHeight = useHeaderHeight();
-
+  const statusBar = Platform.OS === 'android' ? StatusBar.currentHeight : 0;
   const calculateHeight = useCallback(() => {
-    return windowHeight - footerHeight - headerHeight;
-  }, [footerHeight, headerHeight, windowHeight]);
+    return windowHeight - footerHeight - headerHeight + (statusBar ?? 0);
+  }, [footerHeight, headerHeight, windowHeight, statusBar]);
 
   return calculateHeight();
 };
